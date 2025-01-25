224d81c7cf8184a4dadce56d4ff85c15837d75d7
Matezzzz <mrazekmates@gmail.com>
2025-01-25 15:39:09 +0100

    Add dashes and smoother camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //public GameObject objectToFollow;
+
+
+
+
+    //// Start is called once before the first execution of Update after the MonoBehaviour is created
+    //void Start()
+    //{
+    //    float elevation = transform.position.y;
+    //    Vector2 shift = -transform.forward * elevation;
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    Vector2 
+    //}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,9 @@ public class Player : MonoBehaviour
     [Tooltip("The action that fires the gun (Left mouse on PC)")]
     public InputActionReference fireAction;
 
+    [Tooltip("The dash action (Right mouse on PC)")]
+    public InputActionReference dashAction;
+
     [Tooltip("How many units per second the player travels")]
     public float speed = 1f;
 
@@ -17,6 +20,13 @@ public class Player : MonoBehaviour
 
 
     Camera _mainCamera;
+
+    public float dashCooldown = 0f;
+    public float maxDashes = 2f;
+    public float dashDistance = 1f;
+    float _dashAmount = 0f;
+
+    Vector2 _dashDirection;
 
     void Start()
     {
@@ -30,6 +40,8 @@ public class Player : MonoBehaviour
         Vector2 move = moveAction.action.ReadValue<Vector2>();
         transform.position += speed * Time.deltaTime * new Vector3(move.x, 0, move.y);
 
+        if (move.y != 0 || move.x != 0)
+            _dashDirection = move;
 
         //obtain the point where the mouse is pointing at the ground (assumed to be an infinite plane at Y = 0)
         Ray r = _mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
@@ -46,6 +58,17 @@ public class Player : MonoBehaviour
         }
         //gun should fire = mouse is pressed
         gun.ShouldFire = fireAction.action.ReadValue<float>() != 0f;
+
+        if (dashAction.action.triggered)
+        {
+            if (_dashAmount >= dashCooldown)
+            {
+                _dashAmount -= dashCooldown;
+                _dashDirection = _dashDirection.normalized;
+                transform.position += new Vector3(_dashDirection.x, 0, _dashDirection.y) * dashDistance;
+            }
+        }
+        _dashAmount = Mathf.Min(maxDashes, _dashAmount + Time.deltaTime);
     }
 
 
