1050e80c85723cb84c352b3c54a8b29e14a62694
lmeinen <meinen@ubique.ch>
2025-01-26 15:58:20 +0100

    added step sounds

--- Assets/Scripts/PlayerVisuals.cs
+++ Assets/Scripts/PlayerVisuals.cs
@@ -1,10 +1,20 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerVisuals : MonoBehaviour
 {
+
+    public AudioSource audioSource;
+    public List<AudioClip> footstepSounds;
+
     public void AimAt(Vector3 pos)
     {
         Vector3 dif = pos - transform.position;
         transform.rotation = Quaternion.Euler(0, -Mathf.Atan2(dif.z, dif.x) * Mathf.Rad2Deg, 0);
+    }
+
+    public void PlayFootstepSound()
+    {
+        SoundManager.Instance.PlaySound(footstepSounds[Random.Range(0, footstepSounds.Count)], 0.8f, true);
     }
 }
